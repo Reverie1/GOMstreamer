aaea3d43293f0e486514a19794a3ecccf7ebe2f9
Reverie1 <kenton.ta@gmail.com>
2012-01-10 23:01:17 +1100

    Update win/play.cmd

--- win/play.cmd
+++ win/play.cmd
@@ -1,4 +1,4 @@
-@set EMAIL="youremail@example.com"
-@set PASSWORD="PASSWORD"
+@set EMAIL="happygrass_@hotmail.com"
+@set PASSWORD="Qwerty123"
 @set QUALITY="SQTest"
 python ..\gomstreamer.py -e %EMAIL% -p %PASSWORD% -q %QUALITY% %*
